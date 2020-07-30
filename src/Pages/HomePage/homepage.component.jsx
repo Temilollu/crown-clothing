f1e4a887fefa289760f99067f5d7dc33293c0054
Temilollu <temiloluwaodumosu25@gmail.com>
2020-07-30 19:43:01 +0100

    broken into components

---
+++ src/Pages/HomePage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.styles.scss'
+import Directory from '../Components/directory/directory.component'
+
+export const HomePage = () => {
+    return (
+        <div className="homepage">
+           <Directory/>
+        </div>
+    )
+}
